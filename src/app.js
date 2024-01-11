10b459f508e24ae7c6e29e0b59373ba9ecb7202d
falah <falahorganizer@gmail.com>
2024-01-12 01:25:44 +0700

    maffurniture

---
+++ src/app.js
@@ -0,0 +1,77 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Meja Kursi 1", img: "11.jpg", price: 330000 },
+      { id: 2, name: "Meja Kursi 2", img: "12.jpg", price: 350000 },
+      { id: 3, name: "Meja Kursi 3", img: "13.jpg", price: 370000 },
+      { id: 4, name: "Meja Kursi 4", img: "14.jpg", price: 400000 },
+      { id: 5, name: "Meja Kursi 5", img: "15.jpg", price: 450000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika belum ada / cart kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada,cek apakah barang sama atau beda dengan yang ada di cart
+        this.items = this.items.map((item) => {
+          // jika barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah quantity dan totalnya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yang mau di remove berdasarkan id nya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusuri  1 1
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang di klik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barang sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
